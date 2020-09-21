a3d9fc78f78e8cc8d316dd78a87a4f132716a6a6
Tronje Krabbe <hi@tron.je>
2020-09-21 20:07:40 +0200

    Update generate binary to use new Species API
    Signed-off-by: Tronje Krabbe <hi@tron.je>

--- src/bin/generate.rs
+++ src/bin/generate.rs
@@ -1,19 +1,31 @@
-use friendly_zoo::Zoo;
+use friendly_zoo::{Species, Zoo};
 
 enum Arg {
     None,
-    Delimiter,
+    Species,
     NumberOfAdjectives,
 }
 
 struct Args {
-    delimiter: char,
+    species: Species,
     number_of_adjectives: u8,
+}
+
+fn parse_species(s: &str) -> Result<Species, &'static str> {
+    match s.to_ascii_lowercase().as_ref() {
+        "snake" => Ok(Species::Snake),
+        "screaming_snake" => Ok(Species::ScreamingSnake),
+        "camel" => Ok(Species::Camel),
+        "dromedary" => Ok(Species::Dromedary),
+        "kebab" => Ok(Species::Kebab),
+        "screaming_kebab" => Ok(Species::ScreamingKebab),
+        _ => Err("Not a valid species!"),
+    }
 }
 
 fn main() {
     let mut args = Args {
-        delimiter: '-',
+        species: Species::Snake,
         number_of_adjectives: 1,
     };
 
@@ -21,15 +33,15 @@ fn main() {
 
     for arg in std::env::args().skip(1) {
         match arg.as_ref() {
-            "-d" | "--delimiter" => next_arg = Arg::Delimiter,
+            "-s" | "--species" => next_arg = Arg::Species,
             "-n" | "--adjectives" => next_arg = Arg::NumberOfAdjectives,
             value => match next_arg {
                 Arg::None => {
                     eprintln!("Invalid arguments!");
                     return;
                 }
-                Arg::Delimiter => {
-                    args.delimiter = value.parse().unwrap();
+                Arg::Species => {
+                    args.species = parse_species(value).unwrap();
                     next_arg = Arg::None;
                 }
                 Arg::NumberOfAdjectives => {
@@ -42,6 +54,6 @@ fn main() {
 
     println!(
         "{}",
-        Zoo::new(args.delimiter, args.number_of_adjectives).generate()
+        Zoo::new(args.species, args.number_of_adjectives).generate()
     );
 }
